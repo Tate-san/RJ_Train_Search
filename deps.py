51d516bfb9fa958acb7ccf95382ffe57308cdac1
unknown <mateescze@gmail.com>
2019-09-09 01:58:08 +0200

    Upgrades has been made

--- deps.py
+++ deps.py
@@ -5,15 +5,19 @@ import math
 
 
 route_url = 'https://brn-ybus-pubapi.sa.cz/restapi/routes/4908347258/simple?routeId=4908347258&fromStationId=3398241000&toStationId=372825000&tariffs=CZECH_STUDENT_PASS_26'
-dest_url = 'https://www.studentagency.cz/shared/wc/ybus-form/destinations-cs.json'
+dest_url = 'https://brn-ybus-pubapi.sa.cz/restapi/consts/locations'
+#dest_url = 'https://www.studentagency.cz/shared/wc/ybus-form/destinations-cs.json'
+possible_routes_url = 'https://brn-ybus-pubapi.sa.cz/restapi/consts/cityPairs'
 
 dest_list = requests.get(dest_url).json()
 
 ids_list = []
 travels = []
 countries = []
-cities = []
-cities_id = []
+stations = []
+stations_id = []
+tarrifs = [ [ 'REGULAR', 'CZECH_STUDENT_PASS_15', 'CZECH_STUDENT_PASS_26', 'ISIC', 'ATTENDED_CHILD', 'DISABLED', 'DISABLED_ATTENDANCE', 'EURO26', 'CHILD'],
+            [ 'Dospělý 18-64 let', 'Děti a mládež 6 - <18let', 'Student <26 let (žák. průkaz/ISIC)', 'Senior >65 let', 'Doprovázené dítě <6 let', 'ZTP(ZTP/P)', 'Průvodce ZTP/P', 'ISIC >26 let/EYCA/ALIVE', 'Dítě >6-14 let (mez.autobus)', ]]
 
 
 def edit_time(dt):
@@ -35,7 +39,7 @@ def get_trains(from_s, to_s):
         travels.append(requests.get(f'https://brn-ybus-pubapi.sa.cz/restapi/routes/{id}/simple?routeId={id}&fromStationId={from_s}&toStationId={to_s}&tariffs=CZECH_STUDENT_PASS_26').json())
 
             
-def print_data():
+def print_data(date):
 
     seatcount = []
     
@@ -45,7 +49,6 @@ def print_data():
         seatcount = ['0','0','0','0']
         prices = ['0','0','0','0']   
         if 'priceClasses' in t:
-
 
             for seats in t['priceClasses']:
 
@@ -67,24 +70,27 @@ def print_data():
                     
                 
 
-            print( '\n __________________________________________________\n {} \n\n   {} | {} -> {} | {}\n\n {} - {}  ->  {} - {}\n\n    Low cost = {}CZK   FREE SEATS {}\n    Standart = {}CZK   FREE SEATS {}\n    Relax = {}CZK      FREE SEATS {}\n    Business = {}CZK  FREE SEATS {}\n __________________________________________________'.format(
+            print( '\n __________________________________________________\n {} \n\n  {}\n\n{} -> {} | {}\n {} - {}  ->  {} - {}\n\n    {} = {}CZK   FREE SEATS {}\n    {} = {}CZK   FREE SEATS {}\n    {} = {}CZK      FREE SEATS {}\n    {} = {}CZK  FREE SEATS {}\n __________________________________________________'.format(
                 t['sections'][0]['line']['code'],
-                edit_time(t['sections'][0]['departureTime'])[0],
-                edit_time(t['sections'][0]['departureTime'])[1],
+                '.'.join(date),
+                str((edit_time(t['sections'][0]['departureTime'])[1])).rjust(17),
                 edit_time(t['sections'][0]['arrivalTime'])[1],
                 t['sections'][0]['travelTime'],
                 t['sections'][0]['departureCityName'],
                 t['sections'][0]['departureStationName'],
                 t['sections'][0]['arrivalCityName'],
                 t['sections'][0]['arrivalStationName'],
-                prices[0], seatcount[0],
-                prices[1], seatcount[1],
-                prices[2], seatcount[2],
-                prices[3], seatcount[3]
+                'Low cost', prices[0], seatcount[0],
+                'Standart', prices[1], seatcount[1],
+                'Relax', prices[2], seatcount[2],
+                'Business', prices[3], seatcount[3]
                 
                 ))
             seatcount.clear()
             prices.clear()
+
+def prettify(jf):
+    print(json.dumps(jf, indent = 4))
 
         
 def get_dest():
@@ -102,40 +108,92 @@ def get_dest():
         #sel_country = countries[int(cis_c)-1]
 
         # Purge and get city list
-        cities.clear()
-        cities_id.clear()
-        for de in dest_list['destinations'][3]['cities']:
-            for stats in de['stations']:
-                cities.append(stats['fullname'])
-                cities_id.append(stats['id'])
+        stations.clear()
+        stations_id.clear()
 
-        # Print cities & select
-        for i in range(len(cities)):
-            print(i+1,') ', cities[i])
+        for de in dest_list[11]['cities']:
+            
+            for stats in de['stations']:
+                if stats['stationsTypes'][0] == 'TRAIN_STATION':
+                    stations.append(stats['fullname'])
+                    stations_id.append(stats['id'])
+
+        
+
+        # Print stations & select
+        for i in range(len(stations)):
+            print(i+1,') ', stations[i])
             
         cis_m = input('Enter city num: ')
-        sel_city = cities[int(cis_m)-1]   
-        sel_id = cities_id[int(cis_m)-1]  
-        return([sel_city, sel_id])
+        sel_city = stations[int(cis_m)-1]   
+        sel_id = stations_id[int(cis_m)-1]
+
+        for city in dest_list[11]['cities']:
+            for stats in city['stations']:
+                if stats['id'] == sel_id:
+                    city_id = city['id']
+
+        return([sel_city, sel_id, city_id])
 
     # If runs out of array
     except IndexError:
         print('Invalid list index')
 
+def poss_routes(dep_city):
+    possible = []
+    stations.clear()
+    stations_id.clear()
 
+    pos = requests.get(possible_routes_url).json()
+    for poss in pos:
+        for i in range(len(poss['transportTypes'])):
+            if poss['transportTypes'][i] == 'TRAIN':
+                if poss['departureCityId'] == dep_city:
+                    possible.append(poss['arrivalCityId'])
+                    
+    for poss in possible:
+        for city in dest_list[11]['cities']:
+            if poss == city['id']:
+                for stats in city['stations']:
+                    if stats['stationsTypes'][0] == 'TRAIN_STATION':
+                        stations.append(city['name'] +' - '+ stats['name'])
+                        stations_id.append(stats['id'])
+
+    for i in range(len(stations)):
+            print(i+1,') ', stations[i])
+            
+    cis = input('Enter city num: ')
+    
+    sel_id = stations_id[int(cis)-1]
+    sel_stat = stations[int(cis)-1]   
+    return [sel_stat, sel_id]
 
 if __name__ == '__main__':
     if requests.get(route_url).status_code == 200:
         print('From: ')
-        selected_from = get_dest()
+        selected_from = get_dest()       
+        #print(selected_from)
+        #selected_from = ['Brno - Židenice', '3398241000', '10202002']
+        #print(selected_from)
         print('To: ')
-        selected_to = get_dest()
+        selected_to = poss_routes(selected_from[2])
+        #selected_to = ['','372825000'] 
+        #print(selected_to)
         dep_date = (input('[DD.MM.YY] When to departure?: ')).split(".", -1)
-        url = (f'https://brn-ybus-pubapi.sa.cz/restapi/routes/search/simple?locale=cs&departureDate={dep_date[2]}-{dep_date[1]}-{dep_date[0]}&fromLocationId=3398241000&toLocationId=372825000&fromLocationType=STATION&toLocationType=STATION&tariffs=REGULAR')
+        #dep_date = ['05','10','2019']
+
+        for i in range(2):
+            dep_date[i] = "{:02d}".format(int(dep_date[i]))
+        
+          
+        #print(dep_date)
+        
+        url = (f'https://brn-ybus-pubapi.sa.cz/restapi/routes/search/simple?locale=cs&departureDate={dep_date[2]}-{dep_date[1]}-{dep_date[0]}&fromLocationId={selected_from[1]}&toLocationId={selected_to[1]}&fromLocationType=STATION&toLocationType=STATION&tariffs={tarrifs[0][2]}')
         get_ids(url)
         get_trains(selected_from[1],selected_to[1])
+
         # check if route exists
-        print_data()
+        print_data(dep_date)
          
     else:
         print('Page is not responding')
